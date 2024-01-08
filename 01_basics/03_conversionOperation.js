0b558f39c2669a6fc6b4879ae4f82016fdcb8a80
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-01-08 16:53:37 +0530

    Jan 8 Practiced

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,10 @@
+///Outputs for termilal/console window:-
+///console.log(), console.error(), console.warn()
+
+///String interpolation ` ` (backtics)syntax for logging/displaying the calculated values/ variable values
+// console.log(`text ${variable_name}`);
+// console.log(`text ${var1 + var2} text`);
+
 let score = "33";
 // score = "33noo"; // NaN  when converted into Number.
 
@@ -31,7 +38,7 @@ let stringNumber = String(someNumber);
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
 
-// *********************** Operations ***********************
+/// *********************** Operations ***********************
 
 let value = 3;
 let negValue = -value; // -3
@@ -40,9 +47,13 @@ let negValue = -value; // -3
 // console.log(2+2);
 // console.log(2-2);
 // console.log(2*2);
-// console.log(2**3);
-// console.log(2/3);
+// console.log(2**3); ///2 x 2 x 2  = 8
 // console.log(2%3);
+// console.log(2/3);
+
+// console.log(Math.floor(2/3));
+// console.log(Math.ceil(2/3));
+// console.log(Math.round(2/3)); https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
 
 let str1 = "hello";
 let str2 = " hitesh";
@@ -50,32 +61,41 @@ let str2 = " hitesh";
 let str3 = str1 + str2;
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2); ///type coercion => automatic/implicit conversion of values.
+// console.log(1 + "2"); ///type coercion
+// console.log("1" + 2 + 2); ///type coercion
+// console.log(1 + 2 + "2"); ///type coercion, conversion from Number to String, String to Number, Boolean to Number etc
 
-console.log( (3 + 4) * 5 % 3);
+/// *********************** Operator Precedence | (BODMAS rule)-fundamental concept ***********************
+///Brackets, Orders, Division, Multiplication, Addition, and Subtraction ||| (in case multiple power operators in exp. solve them RHS -> LHS)
+///https://www.twinkl.co.in/teaching-wiki/bodmas
 
-// console.log(+true);
-// console.log(+"");
+///General order of solving an expression
+
+console.log(((3 + 4) * 5) % 3); /// 3+4 = 7*5 = 35%3 = 2
+
+// console.log(+true); /// converting boolean into number  kinda trick*
+// console.log(+false); /// converting boolean into number
+// console.log(+""); ///must know which are truthy falsy values in JS
 
 let num1, num2, num3;
 
 num1 = num2 = num3 = 2 + 2;
-// console.log(num1, num2, num3); // lol!
+// console.log(num1, num2, num3); // lol! trash practices
 
 let gameCounter = 100;
 ++gameCounter;
 // console.log(gameCounter);
 
+///Assingment oparators...
+
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
 
-// prefix postfix
+// prefix postfix  must try examples
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
 
 let x2 = 3n;
 const y2 = x2++;
-// console.log(`value of x2: ${x2}, it's type is: ${typeof x2}`);
-// console.log(`value of y2: ${y2}, it's type is: ${typeof y2}`);
+console.log(`value of x2: ${x2}, it's type is: ${typeof x2}`);
+console.log(`value of y2: ${y2}, it's type is: ${typeof y2}`);
--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,3 +1,5 @@
+///MUST KNOW truthy falsy values
+
 // console.log(2 > 1);
 // console.log(2 >= 1);
 // console.log(2 < 1);
@@ -5,16 +7,22 @@
 // console.log(2 != 1);
 
 // console.log("2" > 1);
-console.log("02" > 1);
+// console.log("02" > 1); ///numaric coercian for "2", "02", so at the end js compaired number to number
 
 // console.log(null > 0);
 // console.log(null == 0);
-// console.log(null >= 0);
+// console.log(null >= 0); ///!ntresting
 
 // console.log(undefined == 0);
 // console.log(undefined > 0);
 // console.log(undefined < 0);
 
-// // ===
+// // === strict equality checks !===
+// console.log("2" === 2);
 
-console.log("2" === 2);
+///logical
+// console.log(3 < 5 && 9 < 3) /// false & false
+console.log(4 || 0); /// go deeper into || operator , truthy & falsy
+// console.log(4 && 0)
+// console.log(0 || 4)
+console.log(0 && 4); ///find why this opartion evaluates 0
