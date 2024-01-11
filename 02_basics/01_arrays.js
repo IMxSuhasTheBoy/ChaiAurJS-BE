d3e57077ae91a6ea5a0f6a45316501556540edbb
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-01-11 23:22:49 +0530

    Jan 11 Practiced

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -5,44 +5,44 @@ const myHeors = ["shaktiman", "naagraj"]
 
 const myArr2 = new Array(1, 2, 3, 4)
 
-console.log(myArr2)
+// console.log(myArr2)
 // console.log(myArr[1]);
 
 // -----------Array methods------------
 
 myArr2.push(6)
-console.log(myArr2)
+// console.log(myArr2)
 myArr2.push(7)
-console.log(myArr2)
+// console.log(myArr2)
 myArr2.pop()
-console.log(myArr2)
+// console.log(myArr2)
 myArr2.unshift(9)   // not optimal way
-console.log(myArr2)
+// console.log(myArr2)
 myArr2.shift()
-console.log(myArr2)
+// console.log(myArr2)
 
 // console.log(myArr2.includes(9));
 
 // console.log(myArr2.indexOf(4));
 // console.log(myArr2.indexOf(7));
 
-// const newArr = myArr2.join()
+const newArr = myArr2.join()
 
 // console.log(myArr2);
-// console.log( `${newArr} .join converted an into ${typeof newArr}`);
-
+// console.log( ` .join() returned a ${typeof newArr} ${newArr}`);
+// console.log(newArr)
 
 // slice, splice
 
 console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3)
-console.log(myn1);
+const myn1 = myArr.slice(1, 3) ///returns new array
+console.log(myn1); 
 
-console.log("B ", myArr);
+console.log("B ", myArr); ///original array isnt affected, 
 
 //------------------------
 
 const myn2 = myArr.splice(1, 3)  // returns sub array including del count, changes original array (ref.)
-console.log("C ", myArr); // original array with leftover ele
+console.log("A ", myArr); // original array with leftover ele
 console.log(myn2); // got sub string 
--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,39 +1,54 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"];
 const dc_heros = ["superman", "flash", "batman"];
 
-// marvel_heros.push(dc_heros)  // original arr modified
+marvel_heros.push(dc_heros); // original arr modified
 
 // console.log(marvel_heros);
 // console.log(marvel_heros[3])
 // console.log(marvel_heros[3][1]);
 
+///now comment line 4, then test bellow
 // const allHeros = marvel_heros.concat(dc_heros)  // ret new arr without modifing original arr
 // console.log(allHeros);
 
-// spread / rest---------------
+// console.log(marvel_heros)
+// console.log(dc_heros)
 
-const all_new_heros = [...marvel_heros, ...dc_heros]; // advantage over concat method : multiple arr spread for returning into one arr
+/// spread / rest---------------
+
+// const all_new_heros = [...marvel_heros, ...dc_heros]; /// advantage over concat method : multiple arr spread for returning into one arr
 
 // console.log( all_new_heros);
 
-const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]; ///khichadi Array > nested Array
 
 const real_another_array = another_array.flat(Infinity);
-// console.log(real_another_array);
+console.log(real_another_array);
 
-// console.log(Array.isArray("Hitesh"));
-// console.log(Array.from("Hiteshfddss")); // string is converted in to arr with each indices
-console.log(Array.from({ name: "hitesh" }).values); // interesting  : to make an arr from obj must specify for key / value to have in arr, syntax to be learnt
+console.log(Array.isArray("Hitesh"));
+console.log(Array.isArray(["calm", 25, "calmCalm25"]));
+console.log(Array.from("Hiteshfddss")); /// string is converted in to arr with each indices
+///console.log(Array.from({ name: "hitesh" }).values); /// !nteresting  : to make an arr from obj must specify for key / value to have in arr, syntax to be learnt and modify, not usefull currently
 
 //---------------------
-const set = new Set(["foo", "bar", "baz", "foo"]);
-// console.log(Array.from(set));
-// console.log(Array.of(set));
+const ggSet = new Set(["foo", "bar", "baz", "foso"]);
+
+console.log(ggSet);
+
+// console.log(Array.from(ggSet));
+// console.log(Array.of(ggSet));
 //---------------------
 
-
-let score1 = 100
-let score2 = 200
-let score3 = 300
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
 
 console.log(Array.of(score1, score2, score3));
+
+///[][][][[][][]Array Advavnced[][][][][][][] in jsvuTool.js
+
+/// v8 discussion
+// https://github.com/v8/v8
+/// jsvu : JavaScript (engine) version Updater.
+// https://github.com/GoogleChromeLabs/jsvu#readme
+
