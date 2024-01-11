623743d90b528a4fbdec2b053dc7a714d5eff56f
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-01-11 23:26:39 +0530

    Jan 11 Practiced

---
+++ chai-backend/README.md
@@ -0,0 +1,11 @@
+# ChaiAurBackend
+
+npm pkg
+
+- express
+
+> create route handlers for api req, res etc
+
+- dotenv
+
+> load environment variables from .env file, into process.env
--- chai-backend/index.js
+++ chai-backend/index.js
@@ -1,11 +1,31 @@
-const express = require("express");
+// require("dotenv").config();
+import "dotenv/config";
+// const express = require("express");
+import express from "express";
 const app = express();
-const port = 3000;
+const port = 4000;
 
+///              Route handlers
 app.get("/", (req, res) => {
   res.send("BACKEND Series!");
 });
 
-app.listen(port, () => {
+app.get("/twitter", (req, res) => {
+  res.send("IMxSuhasTheBoy");
+});
+
+app.get("/youtube", (req, res) => {
+  res.send("<h2>Chai Aur code</h2>");
+});
+
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+// console.log(process.env);
+// console.log("Process ID:", process.pid);
+// console.log("Node.js Version:", process.version);
+// console.log("Platform:", process.platform);
+
+// // index.js
+// console.log(process.argv);
